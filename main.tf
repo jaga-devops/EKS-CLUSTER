46b19a58cdb844249602c95209e7f23a6960a17d
Ubuntu <ubuntu@ip-172-31-9-236.us-east-2.compute.internal>
2024-02-07 17:10:55 +0000

    CREATED EKS CLUSTER

---
+++ main.tf
@@ -0,0 +1,240 @@
+provider "aws" {
+      region     = "${var.region}"
+      access_key = "${var.access_key}"
+      secret_key = "${var.secret_key}"
+}
+
+#CREATING 1 VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "OWN-VPC"
+  }
+}
+
+#CREATE PUBLIC SUBNET1
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet1_cidr_block
+  map_public_ip_on_launch = true #THIS WILL ENABLE PUBLIC IP OF INSTANCE WHICH WILL BECREATED IN PUBLIC SUBNET
+  tags = {
+    Name = "PUBLIC-SUBNET-1"
+  }
+}
+
+#CREATE PRIVATE SUBNET1
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet1_cidr_block
+
+  tags = {
+    Name = "PRIVATE-SUBNET-1"
+  }
+}
+#CREATE INTERNET GATEWAY
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "OWN-main"
+  }
+}
+
+#CREATING ELASTIC IP WHICH WILL BW ASSOCIATED WITH NAT GATEWAY
+resource "aws_eip" "EIP1" {
+  tags = {
+    Env = "dev"
+  }
+}
+
+#CREATE NAT GATEWAY IN PUBLIC SUBNET 
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.EIP1.id #NATGATEWAY SHOULD HAVE ELASTIC IP
+  subnet_id     = aws_subnet.public1.id #VVIMP IT's ALWAYS RECOMMENDED THAT NAT GATEWAY SHOULD BE CREATED IN PUBLIC SUBNET
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+#CREATING PUBLIC ROUTE TABLE
+resource "aws_route_table" "publicRT" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "PUBLIC-RT-1"
+  }
+}
+
+#CREATING ROUTE FOR PUBLIC ROUTE TABLE
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.publicRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+#SUBNET ASSOCIATE WITH ROUTE OF PUBLIC ROUTE TABLE
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.publicRT.id
+}
+
+#CREATING PRIVATE ROUTE TABLE
+resource "aws_route_table" "privateRT" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "PRIVATE-RT-1"
+  }
+}
+
+#CREATING ROUTE FOR PRIVATE ROUTE TABLE
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.privateRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id             = aws_nat_gateway.example.id
+}
+
+#SUBNET ASSOCIATE WITH ROUTE OF PRIVATE  ROUTE TABLE
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.privateRT.id
+}
+
+#CREATINGROLE FOR EKS CLUSTER
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "dev-eks-cluster-role" {
+  name               = "dev-eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "dev-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.dev-eks-cluster-role.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "dev-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.dev-eks-cluster-role.name
+}
+resource "aws_eks_cluster" "dev-eks" {
+  name     = "dev-eks"
+  role_arn = aws_iam_role.dev-eks-cluster-role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public1.id, aws_subnet.private1.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.dev-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.dev-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.dev-eks.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.dev-eks.certificate_authority[0].data
+}
+resource "aws_iam_role" "dev-eks-node-role" {
+  name = "dev-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dev-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.dev-eks-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "dev-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.dev-eks-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "dev-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.dev-eks-node-role.name
+}
+
+#EKS-NODE-Groups
+
+resource "aws_eks_node_group" "dev-eks_node_group" {
+  cluster_name    = aws_eks_cluster.dev-eks.name
+  node_group_name = "dev-eks_node_group"
+  node_role_arn   = aws_iam_role.dev-eks-node-role.arn
+  subnet_ids      = [aws_subnet.public1.id, aws_subnet.private1.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.dev-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.dev-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.dev-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+#EKS Node Security Group
+
+resource "aws_security_group" "eks_nodes-sg" {
+  name        = "eks_nodes-sg"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
